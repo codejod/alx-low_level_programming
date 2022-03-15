e9d027356e6685d2ac24e00221aed463dcd6e39b
codejod <codejod@gmail.com>
2022-03-15 15:17:50 -0700

    submit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+/**
+ * _isalpha - checks for alphabets
+ * @c:the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a'  && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
