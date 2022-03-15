4381520bdca7266f90dd0b3b905f52eaaeb1c28c
codejod <codejod@gmail.com>
2022-03-15 15:40:57 -0700

    submit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - computes
+ * @n: the int to check
+ * Return: the absolute value of unt
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
