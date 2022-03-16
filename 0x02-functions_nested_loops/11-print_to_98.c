d653eebea19f8bdb71b342e70e20a9732c3760dc
codejod <codejod@gmail.com>
2022-03-15 17:28:51 -0700

    submit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,15 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * print_to_98 - print to 98
- * @n : number to start from
- * Return:0 or 1
+ * main - check the code for Holberton School students.
+ * Return: Always 0.
  */
-void print_to_98(int n)
+int main(void)
 {
-	while (n < 98)
-	{
-		printf("%i, ", n);
-		n++;
-	}
-	while (n > 98)
-	{
-		printf("%i, ", n);
-		n--;
-	}
-	printf("98");
-	putchar('\n');
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
 }
-
