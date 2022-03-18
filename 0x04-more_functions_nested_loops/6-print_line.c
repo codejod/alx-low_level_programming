d33f0f509bc6bccae01803ecf0ef95af259ba278
codejod <codejod@gmail.com>
2022-03-18 09:21:04 -0700

    submit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,55 +1,16 @@
  #include "main.h"
-
-
-
 /**
-
- * print_square - prints a square
-
- * @size: the size of the square
-
- *
-
- * Return: no return
-
+ * print_line - print  a line
+ * @n: numbers of straigh lines
+ * Return: Always 0.
  */
-
-void print_square(int size)
-
+void print_line(int n)
 {
+int l;
 
-int i, j;
-
-
-
-if (size > 0)
-
+for (l = 1; l <= n; l++)
 {
-
-for (i = 0; i < size; i++)
-
-{
-
-for (j = 0; j < size; j++)
-
-{
-
-_putchar(#);
-
+_putchar(95);
 }
-
-_putchar(n);
-
-}
-
-}
-
-else if (size <= 0)
-
-{
-
-_putchar(n);
-
-}
-
+_putchar('\n');
 }
