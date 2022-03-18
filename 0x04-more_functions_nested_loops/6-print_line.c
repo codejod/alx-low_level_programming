362ecd324d556f17d5c6f5048a58f3e73883a36b
codejod <codejod@gmail.com>
2022-03-18 07:30:47 -0700

    submit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_line - check for a digit
  * @n : number of _ to be printed
  * Return:void
  */
+
 void print_line(int n)
 {
 
@@ -15,4 +17,5 @@ void print_line(int n)
 		i++;
 	}
 	_putchar(n);
+
 }
