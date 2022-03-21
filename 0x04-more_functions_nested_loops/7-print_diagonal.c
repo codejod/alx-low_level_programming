c0136547c93e7fcd9a160f594cec55b0487499b5
codejod <codejod@gmail.com>
2022-03-21 15:41:53 -0700

    done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,15 +3,16 @@
 /**
  * print_diagonal - print digonal
  * @n: input value
-  * Description: draws a diagonal line on the terminal
+ *
+ * Description: draws a diagonal line on the terminal
  * Return: Always (0)
  */
 
 void print_diagonal(int n)
 {
-	int count = 0, size
+int count = 0, size;
 
-		if (n > 0)
+if (n > 0)
 {
 while (count < n)
 {
